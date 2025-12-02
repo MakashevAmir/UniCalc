29844af0e89be627d46d3a649efa099a9443bb73
Amir Makashev <a_makashev@utb.cz>
2025-12-02 10:44:30 +0200

    Update

---
+++ android/app/src/main/java/com/unicalc/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.unicalc.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'CurrencyCalc',
+  appId: 'com.unicalc.app',
+  appName: 'UniCalc',
   webDir: 'www'
 };
 
